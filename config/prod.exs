153245b9ab145780bc34e2d8959e17a8be82b501
John Ankanna <enter2code@gmail.com>
2019-07-26 00:34:24 +0530

    Updated environment variables

--- config/prod.exs
+++ config/prod.exs
@@ -2,11 +2,12 @@ use Mix.Config
 
 config :drinkly, Drinkly.Repo,
   ssl: true,
-  pool_size: 15,
-  port: 5432,
-  database: "d4mncl7664siva",
-  username: "pedfekzzlqxabq",
-  password: "ed2aaae757306430b21ece62aa5f58222c79442f835b29b1a381188aef4f0f55",
-  hostname: "ec2-174-129-227-205.compute-1.amazonaws.com"
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  url: System.get_env("HEROKU_POSTGRESQL_ROSE_URL")
+  # port: 5432,
+  # database: "d4mncl7664siva",
+  # username: "pedfekzzlqxabq",
+  # password: "ed2aaae757306430b21ece62aa5f58222c79442f835b29b1a381188aef4f0f55",
+  # hostname: "ec2-174-129-227-205.compute-1.amazonaws.com"
 
 # write production grade code here
