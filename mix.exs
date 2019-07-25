38def90ef0884cb1b72b887d207ae66d29420532
John Ankanna <enter2code@gmail.com>
2019-07-25 21:42:30 +0530

    updated project info

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Drinkly.MixProject do
       app: :drinkly,
       version: "0.1.0",
       elixir: "~> 1.8",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
