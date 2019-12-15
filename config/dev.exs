8312e2a7e70af2dc70801edb42529bb2df8385c8
blackode <mrblackode@gmail.com>
2019-12-15 13:11:26 +0530

    Updated configurations

--- config/dev.exs
+++ config/dev.exs
@@ -5,3 +5,9 @@ config :drinkly, Drinkly.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost"
+
+config :puppeteer_pdf, exec_path: Path.absname("assets/node_modules/.bin/puppeteer-pdf")
+
+config :drinkly,
+  report_html_template: Path.join(File.cwd!(), "templates/daily_report.html"),
+  report_output_folder: Path.join(File.cwd!(), "pdfs")
